c4cc681d84c6d3d91c0e3538a3e649f3158be2f5
Kotak-Prerit <kotakprerit7@gmail.com>
2023-02-15 21:54:40 +0530

    javascript practice v1

---
+++ app.js
@@ -0,0 +1,28 @@
+//To Print Stuffs
+console.log('app.js is connected')
+console.log('Hi Mom!')
+
+
+//Variables. 3 types of variables(var,let,const)
+//hoisting only works in var(Calling variable before declaring it. )
+var FirstName = 'lokesh'
+var FirstName = 'Mukesh'
+var LastName = 'Mathur'
+var Age = 99
+var City = 'Delhi'
+console.log('Full Name : ' + FirstName + ' ' + LastName + '\n' + 'Age : ' + Age + '\n' + 'City : ' + City)
+
+//let(cannot declare the same var_name once it is declared)
+let firstname = 'Lakhmandas'
+let lastname = 'machhiwala'
+let age = 81
+let city = 'Manipur'
+console.log('Full Name : ' + firstname + ' ' + lastname + '\n' + 'Age : ' + age + '\n' + 'City : ' + city)
+
+//const -> Constant(cannot update the value of a variable once it is created)
+const firstName = 'Danbee'
+const lastName = 'Shin'
+const life = 32
+const CiTy = 'Singapore'
+console.log('Full Name : ' + firstName + ' ' + lastName + '\n' + 'Age : ' + life + '\n' + 'City : ' + CiTy)
+
